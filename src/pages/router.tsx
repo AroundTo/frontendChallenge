ed35343b3b69da7cdd5e0da369040f2f2a345ccb
Italo Nabuco <italonabuco@hotmail.com>
2023-07-20 23:11:36 -0300

    creates NotFoundPage

--- src/pages/router.tsx
+++ src/pages/router.tsx
@@ -5,14 +5,15 @@ import {
 } from 'react-router-dom'
 import ViewProducts from './products/ViewProducts'
 import RootLayout from '../layouts/RootLayout'
+import NotFoundPage from './NotFoundPage'
 
 export const router = createBrowserRouter(
   createRoutesFromElements(
     <Route element={<RootLayout />} path="/">
-      <Route errorElement={<div>Not Found Page</div>}>
+      <Route errorElement={<NotFoundPage />}>
         <Route index element={<ViewProducts />} />
         <Route element={<div>List reviews</div>} path="reviews" />
-        <Route element={<div>Not Found Page</div>} path="*" />
+        <Route element={<NotFoundPage />} path="*" />
       </Route>
     </Route>
   )
