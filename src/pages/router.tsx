b3602aae01409c919a62d7f9488a7e6c9dcf1ab4
Italo Nabuco <italonabuco@hotmail.com>
2023-07-20 23:08:08 -0300

    creates RootLayout layout

--- src/pages/router.tsx
+++ src/pages/router.tsx
@@ -1,21 +1,14 @@
 import {
-  Outlet,
   Route,
   createBrowserRouter,
   createRoutesFromElements,
 } from 'react-router-dom'
 import ViewProducts from './products/ViewProducts'
+import RootLayout from '../layouts/RootLayout'
 
 export const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route
-      element={
-        <div>
-          <Outlet />
-        </div>
-      }
-      path="/"
-    >
+    <Route element={<RootLayout />} path="/">
       <Route errorElement={<div>Not Found Page</div>}>
         <Route index element={<ViewProducts />} />
         <Route element={<div>List reviews</div>} path="reviews" />
