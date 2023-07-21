95d2151ccc5e09498ac38ddc4080ed312922e0df
Italo Nabuco <italonabuco@hotmail.com>
2023-07-20 22:45:06 -0300

    set up tailwindcss - create default styles fontFamily, card, primary-color

--- src/pages/products/ViewProducts/index.tsx
+++ src/pages/products/ViewProducts/index.tsx
@@ -2,7 +2,16 @@ import { useItems } from '../../../graphql/hooks'
 
 const ViewProducts = () => {
   const { items } = useItems()
-  return <ol>{items?.map(item => <li key={item?.name}>{item?.name}</li>)}</ol>
+  return (
+    <>
+      <h1 className="text-4xl font-bold underline mb-3 text-primary-500 m-4">
+        List of Products
+      </h1>
+      <ol className="shadow-card m-4 p-4">
+        {items?.map(item => <li key={item?.name}>{item?.name}</li>)}
+      </ol>
+    </>
+  )
 }
 
 export default ViewProducts
